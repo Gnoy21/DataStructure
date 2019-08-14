bf7c14fa69fa69b922d21d58e676c41aca51b602
Minyong <pmy0237@naver.com>
2019-08-14 11:29:20 +0900

    Stack 예제 추가 함수 : Stack 출력, item 추가, item 삭제

---
+++ Stack/Stack.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct stackNode{
+
+    int data;
+    struct stackNode *link;
+
+}stackNode;
+
+stackNode *top  = NULL;
+
+void push(int data){
+
+    stackNode *temp = malloc(sizeof(stackNode));
+    temp->data      = data;
+    temp->link      = top;
+    top             = temp;
+
+}
+
+int pop(){
+
+    int number;
+    stackNode *temp = malloc(sizeof(stackNode));
+    temp            = top;
+    number          = temp->data;
+    top             = temp->link;
+
+    free(temp);
+
+    return number;
+
+}
+
+void printStack(){
+
+    stackNode *temp = malloc(sizeof(stackNode));
+    temp            = top;
+
+    printf("Stack = [");
+
+    while(temp->link != NULL){
+
+        printf("%d, ", temp->data);
+
+        temp    = temp->link;
+
+    }
+
+    printf("%d]\n", temp->data);
+
+}
+
+int main(){
+
+    push(10);
+    push(20);
+    push(30);
+    printStack();
+
+}
